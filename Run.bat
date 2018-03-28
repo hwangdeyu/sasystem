0f3702f570db437278db562b101be5bfe061912a
huangdeyu <792543411@qq.com>
2018-03-28 20:58:28 +0800

    去掉追踪config.py run.bat

--- Run.bat
+++
@@ -1,5 +0,0 @@
-start mongod.exe --port 65521 --dbpath DBData --auth
-start D:\Softwares\Python2.7\python Run.py
-start D:\Softwares\Python2.7\python aider/Aider.py
-start D:\Softwares\Python2.7\python nascan/NAScan.py
-start D:\Softwares\Python2.7\python vulscan/VulScan.py
